8b4bc2fb37a2d84cb5729b0493e4f93bf1204f27
AgustinVillavicencio17 <129917381+AgustinVillavicencio17@users.noreply.github.com>
2023-05-13 17:35:27 -0300

    Borrando comentarios con dudas resueltas

--- main.py
+++ main.py
@@ -16,7 +16,4 @@ jugadorDAO.addJugador()
 
 jugador1 = jugadorDAO.getJugadorById(0)
 
-print( jugador1.getNickName() ) #Me da error esta instruccion
-
-#Abra que crear clases para cada tipo de sudoku? y para cada difultad?
-#Soy el agustin, perdon que pregunte boludeces pero todavia estoy aprendiendo python :facepalm:
+print( jugador1.getNickName() )
