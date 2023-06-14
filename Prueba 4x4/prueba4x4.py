f2b0c1215b4276cc88ae99bce547e5ca00ca2c9c
AgustinVillavicencio17 <129917381+AgustinVillavicencio17@users.noreply.github.com>
2023-06-14 13:58:40 -0300

    Mejora de legibilidad

--- Prueba 4x4/prueba4x4.py
+++ Prueba 4x4/prueba4x4.py
@@ -2,21 +2,26 @@ import random
 
 def generar_sudoku():
     # Crea una matriz vacía de 4x4
+    
     sudoku = [[0] * 4 for _ in range(4)]
 
     # Rellena la matriz con números aleatorios del 1 al 4
     numeros = [1, 2, 3, 4]
+    
     random.shuffle(numeros)
 
     for i in range(4):
         for j in range(4):
+            
             sudoku[i][j] = numeros[(i + j) % 4]
 
     return sudoku
 
 def imprimir_sudoku(sudoku):
+    
     for i in range(4):
         for j in range(4):
+            
             if sudoku[i][j] == 0:
                 print("-", end=" ")
             else:
@@ -24,10 +29,13 @@ def imprimir_sudoku(sudoku):
         print()
 
 def resolver_sudoku(sudoku):
+    
     for i in range(4):
         for j in range(4):
+            
             if sudoku[i][j] == 0:
                 for num in range(1, 5):
+                    
                     if validar_movimiento(sudoku, i, j, num):
                         sudoku[i][j] = num
                         if resolver_sudoku(sudoku):
@@ -39,11 +47,13 @@ def resolver_sudoku(sudoku):
 def validar_movimiento(sudoku, fila, columna, numero):
     # Verificar la fila
     for j in range(4):
+        
         if sudoku[fila][j] == numero:
             return False
 
     # Verificar la columna
     for i in range(4):
+        
         if sudoku[i][columna] == numero:
             return False
 
@@ -57,6 +67,7 @@ num_celdas_vacias = random.randint(7, 10)
 
 # Vacía algunas celdas del sudoku
 for _ in range(num_celdas_vacias):
+    
     fila = random.randint(0, 3)
     columna = random.randint(0, 3)
     sudoku[fila][columna] = 0
@@ -70,7 +81,7 @@ print()
 #resolver_sudoku(sudoku)
 
 # Imprime el sudoku resuelto
-print("Sudoku resuelto:")
+print("Sudoku a resolver:")
 imprimir_sudoku(sudoku)
 print()
 
@@ -78,11 +89,14 @@ print()
 print("Complete las celdas vacías:")
 for i in range(4):
     for j in range(4):
+        
         if sudoku[i][j] == 0:
             valido = False
             while not valido:
+                
                 valor = input(f"Ingrese el número para la posición [{i+1}][{j+1}]: ")
                 if valor.isdigit() and 1 <= int(valor) <= 4:
+                    
                     valor = int(valor)
                     if validar_movimiento(sudoku, i, j, valor):
                         sudoku[i][j] = valor
@@ -93,5 +107,7 @@ for i in range(4):
                     print("Ingrese un número válido (1-4). Inténtelo nuevamente.")
 
 # Imprime el sudoku con los valores ingresados por el usuario
+
 print("Sudoku final:")
+
 imprimir_sudoku(sudoku)
