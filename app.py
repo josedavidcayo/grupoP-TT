ff566b7ab29414051abcfdebc23c6429a0bf8a9f
AgustinVillavicencio17 <129917381+AgustinVillavicencio17@users.noreply.github.com>
2023-06-14 14:58:29 -0300

    Actualizacion de app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from controlador.index import index, nuevoTablero, tablerosRandom
+
+app = Flask( __name__ )
+
+
+
+#Rutas para consumir.
+@app.route('/')
+def home():
+    return index()
+    
+@app.route('/nuevo')
+def nuevo():
+    return nuevoTablero()
+
+@app.route('/tableros')
+def tableros():
+    return tablerosRandom()
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
